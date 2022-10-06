1ec7f61916fa56949f0bd1f416dc8df89943b8fd
web3nomad.eth <me@web3nomad.eth>
2022-10-06 14:19:50 +0800

    add timenft detail page

--- lib/ethereum/public.ts
+++ lib/ethereum/public.ts
@@ -7,6 +7,23 @@ export const publicProvider = new ethers.providers.JsonRpcProvider(_publicRpcUrl
 
 const _controllerAddress = process.env.NEXT_PUBLIC_CONTROLLER_ADDRESS ?? '0x0000000000000000000000000000000000000000'
 export const controllerContract = new ethers.Contract(_controllerAddress, [
+  'function tokenURI(uint256 tokenId) view returns (string)',
+  /**
+   * struct TimeToken {
+   *     uint256 valueInWei;
+   *     address topicOwner;
+   *     string topicSlug;
+   *     string arId;
+   *     TimeTokenStatus status;
+   * }
+   */
+  'function timeTokenOf(uint256 tokenId) view returns (tuple(uint256, address, string, string, uint256))',
+  /**
+   * struct TimeTrove {
+   *     string arOwnerAddress;
+   *     uint256 balance;
+   * }
+   */
   'function timeTroveOf(address topicOwner) view returns (tuple(string, uint256))',
   'function mintTimeToken(uint256 valueInWei, address topicOwner, string topicSlug, string arId, bytes signature) payable',
   'function createTimeTrove(string arOwnerAddress, bytes signature)'
---
+++ pages/time/[tokenId].tsx
@@ -0,0 +1,55 @@
+import useSWR from 'swr'
+import type { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import MainLayout from '@/components/layouts/MainLayout'
+import { controllerContract } from '@/lib/ethereum/public'
+
+type PageProps = {
+  tokenId: number
+}
+
+const Page: NextPage<PageProps> = ({ tokenId }) => {
+  const fetcher = async (tokenId: string) => {
+    const [tokenURI, timeToken] = await Promise.all([
+      controllerContract.tokenURI(+tokenId),
+      controllerContract.timeTokenOf(+tokenId),
+    ])
+    const [valueInWei, topicOwner, topicSlug, arId, status] = timeToken
+    return { tokenURI, valueInWei, topicOwner, topicSlug, arId, status }
+  }
+  const { data, isValidating } = useSWR(tokenId.toString(), fetcher, {
+    revalidateOnFocus: false,
+  })
+
+  const { tokenURI, valueInWei, topicOwner, topicSlug, arId, status } = data ?? {}
+
+  return (
+    <MainLayout>
+      <Head>
+        <title>MuseTime</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>tokenURI: {tokenURI}</div>
+        <div>valueInWei: {+valueInWei}</div>
+        <div>topicOwner: {topicOwner}</div>
+        <div>topicSlug: {topicSlug}</div>
+        <div>arId: {arId}</div>
+        <div>status: {+status}</div>
+      </main>
+    </MainLayout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async function ({ query }) {
+  const tokenId = +(query.tokenId as string)
+  return {
+    props: {
+      tokenId
+    }
+  }
+}
+
+export default Page
