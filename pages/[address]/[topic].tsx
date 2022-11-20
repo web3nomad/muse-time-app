a7f05777b23df2c25559435d6e6b21735f3e5135
web3nomad.eth <me@web3nomad.eth>
2022-11-20 17:25:04 +0800

    improve mint page, change confirm/reject params

--- pages/[address]/[topic].tsx
+++ pages/[address]/[topic].tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { ethers } from 'ethers'
 
 import { useTimeToken, useTimeTrove } from '@/lib/ethereum/hooks'
+import { useEthereumContext } from '@/lib/ethereum/context'
 import { publicProvider } from '@/lib/ethereum/public'
 
 import type { TopicData, ProfileData } from '@/lib/arweave'
@@ -28,6 +29,7 @@ type PageProps = {
 }
 
 const Page: NextPage<PageProps> = ({ addressSlug, topicOwner, topicId }) => {
+  const { walletAddress, authToken, login } = useEthereumContext()
   const { timeTrove } = useTimeTrove(topicOwner)
   const { mintTimeToken, isMinting, timeTokenMintedLogs } = useTimeToken(topicOwner, topicId)
   const [topic, setTopic] = useState<TopicData|null>(null)
@@ -91,7 +93,7 @@ const Page: NextPage<PageProps> = ({ addressSlug, topicOwner, topicId }) => {
           className="w-32 h-32 bg-neutral-100 bg-no-repeat bg-center bg-contain rounded-full"
           style={profile.avatar ? {backgroundImage:`url(${profile.avatar})`} : {}}
         ></div>
-        <div className="ml-6 lg:ml-0 flex flex-col justify-start items-start lg:items-center">
+        <div className="w-32 ml-6 lg:ml-0 flex flex-col justify-start items-start lg:items-center">
           <div className="font-din-alternate mt-2">{profile.name}</div>
           <div className="text-xs text-neutral-400 font-din-alternate">
             {topicOwner.toLowerCase().replace(/0x(\w{4})\w+(\w{4})/, '0x$1...$2')}
@@ -103,13 +105,22 @@ const Page: NextPage<PageProps> = ({ addressSlug, topicOwner, topicId }) => {
               target="_blank" rel="noreferrer" className="ml-1"
             >{profile['com.twitter']}</a>}
           </div>
-          <button
-            className="my-2 py-2 px-8 text-sm w-full rounded text-white bg-orange-tangelo hover:bg-orange-tangelo/90 flex items-center justify-center"
-            disabled={isMinting} onClick={() => mintTimeToken()}
-          >
-            <span>Mint Now</span>
-            {isMinting && <ArrowPathIcon className="h-5 w-5 animate-spin ml-2" />}
-          </button>
+          {walletAddress ? (
+            <button
+              className="my-2 p-2 text-sm w-full rounded text-white bg-orange-tangelo hover:bg-orange-tangelo/90 flex items-center justify-center"
+              disabled={isMinting} onClick={() => mintTimeToken()}
+            >
+              <span>Mint Now</span>
+              {isMinting && <ArrowPathIcon className="h-5 w-5 animate-spin ml-2" />}
+            </button>
+          ) : (
+            <button
+              className="my-2 p-2 text-sm w-full rounded text-white bg-orange-tangelo hover:bg-orange-tangelo/90 flex items-center justify-center"
+              onClick={() => login()}
+            >
+              <span>Login to Mint</span>
+            </button>
+          )}
         </div>
       </div>
     )
